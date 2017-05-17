98d73e67a0c0f55c573a10c0522820d0cd5e3c14
Edd <edoatley@gmail.com>
2017-05-17 20:21:45 +0100

    Initial Commit

---
+++ src/main/java/com/example/json/utils/JsonUtils/JsonUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.example.json.utils.JsonUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonUtilsApplication.class, args);
+	}
+}
